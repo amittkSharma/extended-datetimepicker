16ded98199aa6f8e665b028c1229be5cb13d049b
Asharma <asharma@agtinternational.com>
2017-07-24 17:47:10 +0200

    Updating the demo for the disabled dates, last week is always disabled in demo.

--- js/demo.js
+++ js/demo.js
@@ -7,14 +7,24 @@
       'ngMaterialDatePicker'
     ])
     .controller('DemoCtrl', function ($scope) {
+
+      var startOfWeek = moment().startOf('week');
+      var endOfWeek = moment().endOf('week');
+
+      var days = [];
+      var day = startOfWeek;
+
+      while (day <= endOfWeek) {
+          days.push(day.format("YYYY-MM-DDTHH:mm:ssZ"));
+          day = day.clone().add(1, 'd');
+      }
+
       $scope.date = new Date();
       $scope.time = new Date();
       $scope.dateTime = new Date();
       $scope.minDate = moment().subtract(1, 'month');
       $scope.maxDate = moment().add(1, 'month');
-      $scope.dates = [new Date('2016-11-14T00:00:00'), new Date('2016-11-15T00:00:00'),
-      new Date('2016-11-30T00:00:00'), new Date('2016-12-12T00:00:00'), new Date('2016-12-13T00:00:00'),
-      new Date('2016-12-31T00:00:00')]
+      $scope.dates = days
 
     })
 
